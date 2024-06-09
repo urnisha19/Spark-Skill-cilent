31f4aec3ad78756467c859ff60b7c4e94ad16bfa
urnisha19 <urnisha09@gmail.com>
2024-06-09 16:51:49 +0600

    updated

--- src/Components/LogInPage/firebase.config.js
+++ src/Components/LogInPage/firebase.config.js
@@ -1,9 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBcw7f3BUWkN0CWjI6HUgMXT18uYkTEOc4",
-    authDomain: "sparkskill-5b012.firebaseapp.com",
-    projectId: "sparkskill-5b012",
-    storageBucket: "sparkskill-5b012.appspot.com",
-    messagingSenderId: "341675552685",
-    appId: "1:341675552685:web:9965fd2e8328b8210f3b24"
-  };
-  export default firebaseConfig;
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+export default firebaseConfig;
